f0bf8a6cd9c0663416d90938c76d2bf1b75dd5df
Aman-31-08 <106014694+Aman-31-08@users.noreply.github.com>
2022-05-28 13:41:00 +0530

    Time: 24 ms (9.43%), Space: 15.3 MB (43.28%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
         int n=indices.size();
-        string g;
         int k=0;
         vector<char> v(n,0);
-        // for(int i=0;i<n;i++)
-        // {
-        //     v.push_back(s[i]);
-        // }
+      
         for(int i=0;i<n;i++)
         {
             v[indices[i]]=s[i];
         }
-        for(int i=0;i<n;i++) cout<<v[i];
         for(int i=0;i<n;i++)
             s[k++]=v[i];
         return s;
