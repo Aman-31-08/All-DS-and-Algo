477eac311b2c7077c3f2f3f0b8ae55e9a1e7ff27
Aman-31-08 <106014694+Aman-31-08@users.noreply.github.com>
2022-05-28 10:41:43 +0530

    Time: 42 ms (14.82%), Space: 17.9 MB (65.37%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -13,11 +13,11 @@ public:
         // }
         // return v[n];
         int sum = 0; // sum of all numbers initially 0
-        int total = nums.size()*(nums.size() + 1)/2; // total is the sum of all numbers in the array
-        for (auto number : nums) { // iterate through the array
-            sum += number; // add the number to the sum
+        int total = nums.size()*(nums.size() + 1)/2;
+        for (auto number : nums) { 
+            sum += number; 
         }
-        return total - sum; // return the difference between the sum and the total that is the missing number
+        return total - sum; 
     
     }
 };
