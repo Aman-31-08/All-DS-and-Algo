e9ca6a9892f02faa9e4faf13a68d550abc117752
Aman-31-08 <106014694+Aman-31-08@users.noreply.github.com>
2022-05-28 10:41:01 +0530

    Time: 29 ms (43.05%), Space: 18 MB (65.37%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> v;
-        for(int i=0;i<=n;i++)
-            v.push_back(i);
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]!=v[i])
-                return v[i];
+        // int n=nums.size();
+        // vector<int> v;
+        // for(int i=0;i<=n;i++)
+        //     v.push_back(i);
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(nums[i]!=v[i])
+        //         return v[i];
+        // }
+        // return v[n];
+        int sum = 0; // sum of all numbers initially 0
+        int total = nums.size()*(nums.size() + 1)/2; // total is the sum of all numbers in the array
+        for (auto number : nums) { // iterate through the array
+            sum += number; // add the number to the sum
         }
-        return v[n];
+        return total - sum; // return the difference between the sum and the total that is the missing number
+    
     }
 };
