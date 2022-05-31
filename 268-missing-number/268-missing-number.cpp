d509ba03b9e6cb2387f221005bc9d49c8138a58d
Aman-31-08 <106014694+Aman-31-08@users.noreply.github.com>
2022-05-31 09:26:17 +0530

    Time: 34 ms (31.29%), Space: 19.4 MB (5.79%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        // int n=nums.size();
-        // vector<int> v;
-        // for(int i=0;i<=n;i++)
-        //     v.push_back(i);
-        // sort(nums.begin(),nums.end());
-        // for(int i=0;i<n;i++)
-        // {
-        //     if(nums[i]!=v[i])
-        //         return v[i];
-        // }
-        // return v[n];
-        int sum = 0;
-        int total = nums.size()*(nums.size() + 1)/2;
-        for (auto number : nums) { 
-            sum += number; 
+        int n=nums.size();
+        vector<int> v;
+        for(int i=0;i<=n;i++)
+            v.push_back(i);
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=v[i])
+                return v[i];
         }
-        return total - sum; 
+        return v[n];
+        // int sum = 0;
+        // int total = nums.size()*(nums.size() + 1)/2;
+        // for (auto number : nums) { 
+        //     sum += number; 
+        // }
+        // return total - sum; 
     
     }
 };
