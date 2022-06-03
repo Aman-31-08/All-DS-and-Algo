37a67316eea4fa2f2de36879fa21323a55737775
Aman-31-08 <106014694+Aman-31-08@users.noreply.github.com>
2022-06-03 23:49:52 +0530

    Time: 8 ms (5.88%), Space: 6.4 MB (25.82%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.cpp
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -2,13 +2,19 @@ class Solution {
 public:
     int numJewelsInStones(string s1, string s2) {
         int c=0;
-        for(int i=0;i<s1.length();i++)
-        {
-            for(int j=0;j<s2.length();j++)
-            {
-                if(s1[i]==s2[j])
-                    c++;
-            }
+        // for(int i=0;i<s1.length();i++)
+        // {
+        //     for(int j=0;j<s2.length();j++)
+        //     {
+        //         if(s1[i]==s2[j])
+        //             c++;
+        //     }
+        // }
+        unordered_map<char,int> m;
+        for(auto it:s2)
+            m[it]++;
+        for(auto it:s1){
+            if(m.count(it)) c+=m[it];
         }
         return c;
     }
