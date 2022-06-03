db067e6495e7374ad76937505751c3630d07d592
Aman-31-08 <106014694+Aman-31-08@users.noreply.github.com>
2022-06-03 23:21:17 +0530

    Time: 22 ms (52.30%), Space: 10.8 MB (52.72%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,33 +1,48 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-          //O(n^2)
-    //     int n=nums.size();
+    //       //O(n^2)
+    // //     int n=nums.size();
+    // //     vector<int> v;
+    // //     for(int i=0;i<n;i++)
+    // //     {
+    // //         for(int j=0;j<n;j++)
+    // //         {
+    // //             if(i!=j&&nums[i]+nums[j]==target)
+    // //             {   v.push_back(i);
+    // //                 //v.push_back(j);
+    // //                 break;
+    // //             }
+    // //         }
+    // //     }
+    // //     return v;
+    //     unordered_map<int,int> mp;
     //     vector<int> v;
-    //     for(int i=0;i<n;i++)
+    //     for(int i=0;i<nums.size();i++)
     //     {
-    //         for(int j=0;j<n;j++)
+    //         if(mp.find(target-nums[i])!=mp.end())
     //         {
-    //             if(i!=j&&nums[i]+nums[j]==target)
-    //             {   v.push_back(i);
-    //                 //v.push_back(j);
-    //                 break;
-    //             }
+    //             v.push_back(mp[target-nums[i]]);
+    //             v.push_back(i);
+    //             return v;
     //         }
+    //         else
+    //             mp[nums[i]]=i;
     //     }
     //     return v;
-        unordered_map<int,int> mp;
+        int n=nums.size();
+        unordered_map<int,int> m;
         vector<int> v;
-        for(int i=0;i<nums.size();i++)
+        for(int i=0;i<n;i++)
         {
-            if(mp.find(target-nums[i])!=mp.end())
+            if(m.find(target-nums[i])!=m.end())
             {
-                v.push_back(mp[target-nums[i]]);
+                v.push_back(m[target-nums[i]]);
                 v.push_back(i);
                 return v;
             }
-            else
-                mp[nums[i]]=i;
+            else m[nums[i]]=i;
+            
         }
         return v;
     }
