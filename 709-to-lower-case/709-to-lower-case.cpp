e4af278b118212721c97220a7e2d6942a35ad086
Aman-31-08 <106014694+Aman-31-08@users.noreply.github.com>
2022-06-18 20:18:12 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (8.12%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.cpp
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -1,6 +1,11 @@
 class Solution {
-    public String toLowerCase(String s) {
-        
-        return s.toLowerCase();
+public:
+    string toLowerCase(string s) {
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]<=90 and s[i]>=65)
+                s[i]+=32;
+        }
+        return s;
     }
-}
+};
