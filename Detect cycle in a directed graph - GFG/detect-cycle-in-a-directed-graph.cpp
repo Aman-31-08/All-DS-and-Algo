bb645a17271a546d9d6a77678ea40546d94d83bf
Aman-31-08 <106014694+Aman-31-08@users.noreply.github.com>
2022-06-21 14:32:42 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -8,28 +8,29 @@ class Solution {
     // Function to detect cycle in a directed graph.
     int dfs(vector<int> adj[],int s,vector<int> &vis,vector<int> &temp)
     {
-        vis[s]=1;
         temp[s]=1;
+        vis[s]=1;
         for(auto x:adj[s])
         {
             if(!vis[x])
             {
-                int ans=dfs(adj,x,vis,temp);
-                if(ans==1) return 1;
-                
+                int c=dfs(adj,x,vis,temp);
+                if(c==1) return true;
             }
-            else if(temp[x]) return 1;
+            else if(temp[x]) return true;
         }
         temp[s]=0;
-        return 0;
+        return false;
     }
     bool isCyclic(int V, vector<int> adj[]) {
         vector<int> vis(V,0);
         vector<int> temp(V,0);
-        for(int i=0;i<V;i++){
-            if(!vis[i]){
-                int c=dfs(adj,i,vis,temp);
-                if(c==1) return true;
+        for(int i=0;i<V;i++)
+        {
+            if(!vis[i])
+            {
+                int x=dfs(adj,i,vis,temp);
+                if(x==1) return true;
             }
         }
         return false;
